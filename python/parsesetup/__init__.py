2ec8c38440c40e163cda74f40800965920aef31a
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-09-06 07:31:07 -0400

    loading parse with venues

--- python/parsesetup/__init__.py
+++ python/parsesetup/__init__.py
@@ -1,6 +1,10 @@
+import tweetanalysis.analyzer
 import foursquare
-from parse_rest.connection import register
+import json
+import models
 import parsesetup_cfg
+from parse_rest.connection import register
+from parse_rest import query
 
 FS_CLIENT_ID = parsesetup_cfg.FS_CLIENT_ID
 FS_CLIENT_SECRET = parsesetup_cfg.FS_CLIENT_SECRET
@@ -12,9 +16,30 @@ PARSE_MASTER_KEY = parsesetup_cfg.PARSE_MASTER_KEY
 register(PARSE_APPLICATION_ID, PARSE_REST_API_KEY,
          master_key=PARSE_MASTER_KEY)
 
+
 def main():
 
     client = foursquare.Foursquare(client_id=FS_CLIENT_ID, client_secret=FS_CLIENT_SECRET)
-    # print client.venues.search(params={'ll': '40.7,74.0', 'limit': '10'})
+    places = client.venues.search(params={'ll': '40.7,-74.0', 'limit': '100'})['venues']
 
-# main()
+    for place in places:
+        print place
+        name = place['name']
+        try:
+            category = place['categories'][0]['shortName']
+        except IndexError, KeyError:
+            category = None
+        pop = 10
+        geotag = tweetanalysis.analyzer.GeoTag(place['location']['lat'], place['location']['lng'])
+        addr = ' '.join(place['location']['formattedAddress'])
+
+        if category:
+            try:
+                models.Venue.Query.get(name=name)
+                print '{} already exists'.format(name)
+            except query.QueryResourceDoesNotExist:
+                venue = models.Venue.build(name=name, category=category, pop=pop, geotag=geotag, addr=str(addr))
+                venue.save()
+
+
+main()
