e92502bc6e7f695a73a341a20ae93b19dab176ea
Patrick <pscollins@uchicago.edu>
2014-09-06 03:55:58 -0500

    adding models

--- python/parsesetup/__init__.py
+++ python/parsesetup/__init__.py
@@ -1,5 +1,5 @@
 import foursquare
-import parse_rest
+from parse_rest.connection import register
 import parsesetup_cfg
 
 FS_CLIENT_ID = parsesetup_cfg.FS_CLIENT_ID
@@ -9,10 +9,12 @@ PARSE_APPLICATION_ID = parsesetup_cfg.PARSE_APPLICATION_ID
 PARSE_REST_API_KEY = parsesetup_cfg.PARSE_REST_API_KEY
 PARSE_MASTER_KEY = parsesetup_cfg.PARSE_MASTER_KEY
 
+register(PARSE_APPLICATION_ID, PARSE_REST_API_KEY,
+         master_key=PARSE_MASTER_KEY)
 
 def main():
 
     client = foursquare.Foursquare(client_id=FS_CLIENT_ID, client_secret=FS_CLIENT_SECRET)
     # print client.venues.search(params={'ll': '40.7,74.0', 'limit': '10'})
 
-main()
+# main()
