484a0adfcf9f16835ac855fbf98f212e2ec158ab
Hikari Senju <hikari@587213130.guest.umich.net>
2014-09-06 01:26:51 -0400

    added iOS folder

---
+++ iOS/Goober/HSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HSAppDelegate.h
+//  Goober
+//
+//  Created by Hikari Senju on 9/6/14.
+//  Copyright (c) 2014 Hikari Senju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
